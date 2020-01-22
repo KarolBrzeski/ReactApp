5a4037065f6586e9cd8810e667e764a9edcb6e40
Karol Brzeski <karol.brzeski91@gmail.com>
2020-01-22 12:30:40 +0100

    Add two buttons with props

--- src/Views/Root/Root.js
+++ src/Views/Root/Root.js
@@ -4,7 +4,7 @@ import Button from 'components/Button/Button';
 const Root = () => (
   <div>
     <h1>Hello react</h1>
-    <Button>Btn first</Button>
+    <Button height="50px">Btn first</Button>
     <Button secondary>Btn second</Button>
   </div>
 );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${props => (props.secondary ? '#e6e6e6' : '#ffd82b')};
-  width: ${props => (props.secondary ? '105px' : '220px')};
-  height: ${props => (props.secondary ? '30px' : '47px')};
+  background-color: #ffd82b;
+  width: 220px;
+  height: ${({ height }) => height || '45px'};
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat';
   font-weight: 500px;
-  font-size: ${props => (props.secondary ? '10px' : '16px')};
+  font-size: 16px;
   text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: #e6e6e6;
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
 `;
 
 export default Button;
