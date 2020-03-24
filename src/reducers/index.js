cbc93d5b47e1ab2635082745050497bbcd3ee1fd
Karol Brzeski <karol.brzeski91@gmail.com>
2020-03-24 13:51:25 +0100

    Conect redux with Notes component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -67,6 +67,36 @@ const initialState = {
       created: '10 days',
     },
   ],
+  notes: [
+    {
+      id: 1,
+      title: 'Wake me up when Vue ends',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id: 2,
+      title: 'Como es An Gular?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id: 3,
+      title: 'Du bist Reactish',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+    },
+    {
+      id: 4,
+      title: 'Reactuj się kto moze!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+    },
+  ],
 };
 
 const rootReducer = (state = initialState, action) => {
